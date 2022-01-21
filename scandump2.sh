248b02c6b945909194be81b7e374df6d8d072457
Paul Evans <49246507+DriedMangoBananas@users.noreply.github.com>
2022-01-21 13:02:00 +0100

    Update scandump2.sh

--- scandump2.sh
+++ scandump2.sh
@@ -1,28 +1,31 @@
 
 
 #scandump2.sh
-#Get information About a Host and Network and Dump to file
+#Quick information About a Host and Network and Dump to file
 
 
 # Find local IP from ifconfig.
 localIP=($(sudo ifconfig | grep "inet " | awk '{print $2}' | awk 'END{print}' ))
 
-# Add wildscard.
+# Add wildcards.
 startIP=`echo $localIP | cut -d "." -f1-3`
 searchIP=`echo "$startIP.*"`
 echo "Search IP is: $searchIP"
 sleep 1
 
-read -p '[ ? ] Enter Search IP: ' ip
+# Prompt and run nmap scan on IP or range
+read -p '[ ? ] Enter Search IP or range: ' ip
 read -p '[ ? ] Save results as: ' file
 echo "[ i ] Results will be saved as $file.txt"
-echo "[ i ] Nmap will ask for sudo then scan the IP $ip..."
+echo "[ i ] Nmap run as sudo to scan the IP $ip..."
 nmap $ip -v0 -sV -n -oN $file.txt
-echo [ ✔ ] Scan complete. Showing results...
+echo "[ i ] Scan complete. Showing results..."
 sleep 1
 echo                                  
-echo Now showing acive connections on host 
+echo "[ i ] Now showing acive connections on host"
 echo 
+
+# Run netstat on host for more information
 netstat -plunt >> $file.txt
 sleep 1
 cat "$file.txt"
