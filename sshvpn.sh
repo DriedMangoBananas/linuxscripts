0c7258e8b371fcaa3fdae86d37724a0452645cc6
Paul Evans <49246507+DriedMangoBananas@users.noreply.github.com>
2022-01-23 21:03:12 +0100

    Update sshvpn.sh

--- sshvpn.sh
+++ sshvpn.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-##Script to automate VPN over SSH connection to a VPS using sshuttle##
-    
+# Script to make VPN over SSH connection to a VPS using sshuttle
+
+# redirect stdout/stderr to a logfile for monitoring
+exec &>~/Desktop/logfile.txt
+
+# make connection
 sshuttle --dns -r paul@ipaddress  0.0.0.0/0 
